5cba8b12602e66304e6b2815b5e906d13d8fbb2f
jeremyOh <jeremy7973@gmail.com>
2023-02-06 20:37:20 +0900

    prettier, eslint add in workSpace

--- packages/express-middle/app/controller/CommonController.ts
+++ packages/express-middle/app/controller/CommonController.ts
@@ -1,25 +1,25 @@
-import express from 'express';
+import express from 'express'
 
 /**
  * test
  * @author jeremyOh
  */
-class Controller{ 
-    public application : express.Application
+class Controller {
+  public application: express.Application
 
-    constructor(){
-        this.application = express()
-        this.router()
-    }
+  constructor() {
+    this.application = express()
+    this.router()
+  }
 
-    /**
-     * @author jeremyOh
-     */
-    private router(): void{ 
-        this.application.get("/",(req: express.Request, res: express.Response) => {
-            res.send('HELLO_WORLD')
-        })
-    }
+  /**
+   * @author jeremyOh
+   */
+  private router(): void {
+    this.application.get('/', (req: express.Request, res: express.Response) => {
+      res.send('HELLO_WORLD')
+    })
+  }
 }
 
-export default Controller;
+export default Controller
